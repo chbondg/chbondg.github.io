1ec697ff314cdf5706714391a319a5a456780d20
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-04 15:00:25 +0800

    20250804 v1

--- git-blogwithhexo.bat
+++ git-blogwithhexo.bat
@@ -2,7 +2,7 @@ cd "C:\Users\chbon\Documents\blogwithhexo"
 git init
 git add .
 git commit -m "20250804 v1"
-git remote add origin https://github.com/chbondg/chbondg.github.io.git
+git remote add origin https://github.com/ma9in3/ma9in3.github.io.git
 git checkout -b main
 git push -u origin main --force
 explorer  http://localhost:4000
